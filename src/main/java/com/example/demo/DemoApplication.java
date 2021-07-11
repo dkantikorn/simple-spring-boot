9a03da529836986002d490cc8741c685d055e801
DK_Antikorn <sarawutt.b@pakgon.com>
2021-07-11 13:23:26 +0700

    edit main form

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
