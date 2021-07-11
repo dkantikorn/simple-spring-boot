b884e7e4a1a8f24088a05e9c1765a403d9507989
DK_Antikorn <sarawutt.b@pakgon.com>
2021-07-11 17:19:38 +0700

    add query update student

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.student.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,8 @@ public interface StudentRepository extends JpaRepository<Students, Long> {
 
     @Query("SELECT s FROM Students s WHERE s.email = ?1")
     Optional<Students> findStudensByEmail(String email);
+
+//    @Modifying //ถ้ามีการแก้ไขเปลี่ยนแปลงข้อมูลต้องมี @Modifying เสมอ เช่นการ update หรือ delete
+//    @Query("UPDATE Students student SET student.deleted = ?2 WHERE nws = ?1")
+//    public void updateForStudent(Students student, Boolean deleted);
 }
